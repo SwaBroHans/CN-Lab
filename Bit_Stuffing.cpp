cb1aaf731d15e68ebacfc515392fad91f1c6e311
SwaBroHans <anubhav14961@gmail.com>
2026-04-10 09:47:32 +0530

    Update Bit_Stuffing.cpp

--- Bit_Stuffing.cpp
+++ Bit_Stuffing.cpp
@@ -6,26 +6,28 @@ int main()
 	string s = "";
 	cout << "Enter Bit Stream : ";
 	cin >> s;
-	int n = s.length();
+
 	int cnt = 0;
 	string ans = "";
-	for (int i = 0; i < n - 1; i++)
+
+	for (int i = 0; i < s.length(); i++)
 	{
-		if (cnt == 5)
-		{
-			ans = ans + '0';
-			cnt = 0;
-		}
-		if (s[i] == '1' && s[i + 1] == '1')
+		ans += s[i];
+
+		if (s[i] == '1')
 		{
 			cnt++;
+			if (cnt == 5)
+			{
+				ans += '0'; // stuff 0 after five 1s
+				cnt = 0;
+			}
 		}
 		else
 		{
-			cnt = 0;
+			cnt = 0; // reset on 0
 		}
-		ans = ans + s[i];
 	}
-	ans = ans + s[n - 1];
+
 	cout << "\nBit Stuffed : " << ans << endl;
 }
